1963f94ed929f3b423ec34ab1dc88e1164d6fb87
Springer <zxllks@yahoo.com>
2024-05-10 14:30:11 +0800

    05-3: CustomizeDesign part2: change case color

--- src/app/configure/design/CustomizeDesign.tsx
+++ src/app/configure/design/CustomizeDesign.tsx
@@ -1,8 +1,13 @@
 'use client'
 
 import { AspectRatio } from '@/components/ui/aspect-ratio'
+import { Label } from '@/components/ui/label'
+import { ScrollArea } from '@/components/ui/scroll-area'
 import { cn } from '@/lib/utils'
+import { COLORS } from '@/validator/options'
+import { RadioGroup } from '@headlessui/react'
 import NextImage from 'next/image'
+import { useState } from 'react'
 import { Rnd } from 'react-rnd'
 
 interface CustomizeDesignProps {
@@ -11,6 +16,9 @@ interface CustomizeDesignProps {
   imageDimensions: { width: number; height: number }
 }
 export const CustomizeDesign = ({ configId, imageUrl, imageDimensions }: CustomizeDesignProps) => {
+  const [options, setOptions] = useState({
+    color: COLORS[0],
+  })
   // REFACTOR: get rid of unnecessary div and css: aspect-[896/1831] z-index and so on
   return (
     <div className='relative my-20 grid grid-cols-1 pb-20 lg:grid-cols-3'>
@@ -33,8 +41,7 @@ export const CustomizeDesign = ({ configId, imageUrl, imageDimensions }: Customi
           <div
             className={cn(
               'absolute inset-0 bottom-px left-[3px] right-[3px] top-px rounded-[32px]',
-              `bg-[#4e525b]`,
-              // `bg-${options.color.tw}`, // TODO: dynamic phone case color by user choice
+              `bg-${options.color.tw}`, // dynamic phone case color by user choice
             )}
           />
         </div>
@@ -58,6 +65,60 @@ export const CustomizeDesign = ({ configId, imageUrl, imageDimensions }: Customi
           <NextImage src={imageUrl} fill alt='your image' className='pointer-events-none' />
         </Rnd>
       </div>
+      <div className='col-span-full flex h-[37.5rem] w-full flex-col bg-white lg:col-span-1'>
+        <ScrollArea className='relative flex-1 overflow-auto'>
+          {/* fading gradient */}
+          <div
+            aria-hidden='true'
+            className='pointer-events-none absolute inset-x-0 bottom-0 z-10 h-12 bg-gradient-to-t from-white'
+          />
+          <div className='px-8 pb-12 pt-8'>
+            <h2 className='text-3xl font-bold tracking-tight'>Customize your case</h2>
+            {/* FIXME: as border */}
+            <div className='my-6 h-px w-full bg-zinc-200' />
+
+            {/* Selection */}
+            <div className='relative mt-4 flex h-full flex-col justify-between'>
+              <div className='flex flex-col gap-6'>
+                <RadioGroup
+                  value={options.color}
+                  onChange={(val) => {
+                    setOptions((prev) => ({
+                      ...prev,
+                      color: val,
+                    }))
+                  }}
+                >
+                  <Label>Color: {options.color.label}</Label>
+                  <div className='mt-3 flex items-center space-x-3'>
+                    {COLORS.map((color) => (
+                      <RadioGroup.Option
+                        key={color.label}
+                        value={color}
+                        className={({ active, checked }) =>
+                          cn(
+                            'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full border-2 border-transparent p-0.5 focus:outline-none focus:ring-0 active:outline-none active:ring-0',
+                            {
+                              [`border-${color.tw}`]: active || checked,
+                            },
+                          )
+                        }
+                      >
+                        <span
+                          className={cn(
+                            `bg-${color.tw}`,
+                            'h-8 w-8 rounded-full border border-black border-opacity-10',
+                          )}
+                        />
+                      </RadioGroup.Option>
+                    ))}
+                  </div>
+                </RadioGroup>
+              </div>
+            </div>
+          </div>
+        </ScrollArea>
+      </div>
     </div>
   )
 }
@@ -73,3 +134,6 @@ const CornerComponent = () => {
 // 2 shadcn: `pnpm dlx shadcn-ui@latest add aspect-ratio`
 // 3 LEARN: Next Image 1.fill 2.whitelist
 // 4 react-rnd: `pnpm add react-rnd`
+// 5 shadcn: `pnpm dlx shadcn-ui@latest add scroll-area`
+// 6 headlessui: `pnpm add @headlessui/react`
+// 7 shadcn: `pnpm dlx shadcn-ui@latest add label`
---
+++ src/validator/options.ts
@@ -0,0 +1,13 @@
+export const COLORS = [
+  { label: 'Black', value: 'black', tw: 'zinc-900' },
+  { label: 'Blue', value: 'blue', tw: 'blue-950' },
+  { label: 'Rose', value: 'rose', tw: 'rose-950' },
+] as const
+
+// NOTES:
+// as const: very useful to make readonly dictionary types
+// LEARN: HOW TO use tailwind dynamic classnames:
+// ANSWER: comment the atomic classnames anywhere in project
+// bg-blue-950 border-blue-950
+// bg-zinc-900 border-zinc-900
+// bg-rose-950 border-rose-950
