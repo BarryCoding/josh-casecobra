80e02a28caf3a478b165f135567309e7d0409d97
Springer <zxllks@yahoo.com>
2024-05-10 12:07:42 +0800

    05-1: db create configuration and design page start

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,5 +1,7 @@
 import { createUploadthing, type FileRouter } from 'uploadthing/next'
 import { z } from 'zod'
+import sharp from 'sharp' // TRY: new version of sharp
+import { db } from '@/db'
 
 const f = createUploadthing()
 
@@ -17,8 +19,29 @@ export const ourFileRouter = {
     })
     .onUploadComplete(async ({ metadata, file }) => {
       const { configId } = metadata.input
+      const imageUrl = file.url // file url stored in uploadthing cloud server
 
-      return { configId }
+      const res = await fetch(imageUrl) // fetch image data from url
+      const arrayBuffer = await res.arrayBuffer() // convert to ArrayBuffer
+      const imgMetadata = await sharp(arrayBuffer).metadata() // get image metadata
+      const { width = 500, height = 500 } = imgMetadata // get image width and height
+
+      if (!configId) {
+        // 1st step/upload: upload page startUpload with {configId:undefined}
+        // db create configuration after image is uploaded
+        const configurationCreated = await db.configuration.create({
+          data: { imageUrl, width, height },
+        })
+        return { configId: configurationCreated.id }
+      } else {
+        // 2nd step/upload: design page startUpload with {configId} from previous step
+        // db update configuration after image is uploaded
+        await db.configuration.update({
+          where: { id: configId },
+          data: { croppedImageUrl: imageUrl },
+        })
+        return { configId }
+      }
     }),
 } satisfies FileRouter
 
